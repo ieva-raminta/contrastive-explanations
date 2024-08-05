12d6b822f4a5d9e8fde2b603a216ac81b9d37f24
Ieva Raminta Staliunaite <ieva.staliunaite@gmail.com>
2024-08-05 15:50:44 +0100

    dont split ids

--- ids.py
+++ ids.py
@@ -11,26 +11,26 @@ for root, dirs, files in os.walk("/home/irs38/Negative-Precedent-in-Legal-Outcom
                 data = json.load(f)
                 case_number = data['case_no']
                 file_number = file.split(".")[0]
-                case_numbers = case_number.split(";")
-                file_number_to_case_number[file_number] = case_numbers
+                #case_numbers = case_number.split(";")
+                file_number_to_case_number[file_number] = case_number
 for root, dirs, files in os.walk("/home/irs38/Negative-Precedent-in-Legal-Outcome-Prediction/ECHR/Outcome/train/"):
     for file in files:
         if file.endswith(".json"):
             with open(os.path.join(root, file)) as f:
                 data = json.load(f)
                 case_number = data['case_no']
-                case_numbers = case_number.split(";")
+                #case_numbers = case_number.split(";")
                 file_number = file.split(".")[0]
-                file_number_to_case_number[file_number] = case_numbers
+                file_number_to_case_number[file_number] = case_number
 for root, dirs, files in os.walk("/home/irs38/Negative-Precedent-in-Legal-Outcome-Prediction/ECHR/Outcome/test/"):
     for file in files:
         if file.endswith(".json"):
             with open(os.path.join(root, file)) as f:
                 data = json.load(f)
                 case_number = data['case_no']
-                case_numbers = case_number.split(";")
+                #case_numbers = case_number.split(";")
                 file_number = file.split(".")[0]
-                file_number_to_case_number[file_number] = case_numbers
+                file_number_to_case_number[file_number] = case_number
 
 # save the dictionary to a file
 with open("/home/irs38/Negative-Precedent-in-Legal-Outcome-Prediction/ECHR/Outcome/numbers.json", "w") as f:
